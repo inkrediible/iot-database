2d44f8d8073fd7d8c676ea42fd39c971d51986d1
inkrediible <tiyasa.paul@gmail.com>
2025-04-06 14:43:41 +0530

    Update sensormodel.js

--- sensormodel.js
+++ sensormodel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+onst mongoose = require("mongoose");
 
 const sensorSchema = new mongoose.Schema({
   soil_moisture: Number,
@@ -7,9 +7,15 @@ const sensorSchema = new mongoose.Schema({
   rain_detected: String,
   water_flow: Number,
   pump_status: String,
-  timestamp: { type: Date, default: Date.now },
+  timestamp: {
+    type: Date,
+    default: () => {
+      const istOffset = 5.5 * 60 * 60 * 1000; // IST 
+      return new Date(Date.now() + istOffset);
+    },
+  },
 });
 
-const Sensor = mongoose.model("Sensor", sensorSchema, "sensor_data"); // Collection name: "sensors"
+const Sensor = mongoose.model("Sensor", sensorSchema, "sensor_data"); // Collection name: "sensor_data"
 
 module.exports = Sensor;
